4d1ba9f18ca1e0f7797932c43564be6c0cce7c0c
thetong <ukkharawat.uim@gmail.com>
2020-03-24 15:31:02 +0700

    add profile model

--- tucovid/account/admin.py
+++ tucovid/account/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from account.models import Profile
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [ field.name for field in Profile._meta.get_fields() ]
+    search_fields = [
+        'full_name',
+        'phone_no',
+    ]
