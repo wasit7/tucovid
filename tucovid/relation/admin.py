4e5a8cf833a6f1a026aeb7956f032f103dda1e79
thetong <ukkharawat.uim@gmail.com>
2020-03-24 16:05:52 +0700

    add relation model

--- tucovid/relation/admin.py
+++ tucovid/relation/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from relation.models import Relationship
 
-# Register your models here.
+@admin.register(Relationship)
+class RelationshipAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'first_person',
+        'second_person',
+        'level',
+        'created_date',
+        'created_by',
+    ]
+
+    def first_person(self, obj):
+        return obj.persons.first()
+    
+    def second_person(self, obj):
+        return obj.persons.last()
--- tucovid/relation/models.py
+++ tucovid/relation/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Relationship(models.Model):
+    FAMILY_RELATION = 'FR'
+    CLOSE_FRIEND_RELATION = 'CR'
+
+    RELATION_LEVELS = [
+        (FAMILY_RELATION, 'Family'),
+        (CLOSE_FRIEND_RELATION, 'Close friend'),
+    ]
+
+    # required exactly 2 persons.
+    persons = models.ManyToManyField(User, related_name='relationship')
+    level = models.CharField(max_length=2, choices=RELATION_LEVELS)
+    created_date = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name='relation_reporter')
+
+    def __str__(self):
+        return 'Relationships of {} and {} is {}'.format(
+            self.persons.first(), self.persons.last(), self.get_level_display()
+        )
