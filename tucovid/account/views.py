7ef939bb5935ef5dfb48bdc79e7eb328f0a83aaa
thetong <ukkharawat.uim@gmail.com>
2020-03-24 17:59:17 +0700

    add login page

--- tucovid/account/views.py
+++ tucovid/account/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
+from account.decorators import is_anonymous
 
-# Create your views here.
+@require_GET
+@is_anonymous
+def login_page(request):
+    return render(request, 'account/login.html')
--- tucovid/tucovid/urls.py
+++ tucovid/tucovid/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('oauth/', include('social_django.urls', namespace='social'))
+    path('oauth/', include('social_django.urls', namespace='social')),
+    path('account/', include('account.urls', namespace='account')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
