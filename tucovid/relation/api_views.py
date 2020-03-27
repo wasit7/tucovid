912994f82cd3f26a9b992c238137604bbdf394e8
thetong <ukkharawat.uim@gmail.com>
2020-03-27 17:46:46 +0700

    when normal user try to get other history, system send empty array

--- tucovid/relation/api_views.py
+++ tucovid/relation/api_views.py
@@ -6,13 +6,17 @@ from relation.relation import get_history_relation, get_event_history
 @require_GET
 @login_required
 def relation_history(request, user_id):
-    relation_history = get_history_relation(user_id)
+    relation_history = []
+    if request.user.is_staff or user_id == request.user.id:
+        relation_history = get_history_relation(user_id)
 
     return JsonResponse(data=relation_history, status=200, safe=False)
 
 @require_GET
 @login_required
 def event_history(request, user_id):
-    event_history = get_event_history(user_id)
+    event_history = []
+    if request.user.is_staff or user_id == request.user.id:
+        event_history = get_event_history(user_id)
 
     return JsonResponse(data=relation_history, status=200, safe=False)
