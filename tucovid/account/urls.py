d41085554b1ef30f8aed0bd089f8b6d66e15b552
thetong <ukkharawat.uim@gmail.com>
2020-03-27 14:31:03 +0700

    add get profile by user id and staff can edit other profile

--- tucovid/account/urls.py
+++ tucovid/account/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from account.views import login_page, profile_page, logout_view
+from account.views import (
+    login_page,
+    profile_page,
+    logout_view,
+    profile_page_by_id
+)
 from account.api_views import search_profile_api
 
 app_name = 'account'
@@ -9,4 +14,5 @@ urlpatterns = [
     path('profile/', profile_page, name='profile_page'),
     path('logout/', logout_view, name='logout'),
     path('search/', search_profile_api, name='search_profile_api'),
+    path('profile/<int:user_id>/', profile_page_by_id, name='profile_page_by_id'),
 ]
