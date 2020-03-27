6cdd874cc7d51347bf6224b3a506f7c1573f154d
thetong <ukkharawat.uim@gmail.com>
2020-03-27 17:55:53 +0700

    fix wrong views

--- tucovid/relation/urls.py
+++ tucovid/relation/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from relation.views import index, relation_page, event_page
-from relation.api_views import relation_history
+from relation.api_views import relation_history, event_history
 
 app_name = 'relation'
 
@@ -9,5 +9,5 @@ urlpatterns = [
     path('relation/', relation_page, name='relation_page'),
     path('event/', event_page, name='event_page'),
     path('relation/history/<int:user_id>/', relation_history, name='relation_history'),
-    path('event/history/<int:user_id>/', event_page, name='event_page'),
+    path('event/history/<int:user_id>/', event_history, name='event_history'),
 ]
