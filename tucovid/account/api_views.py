f1bce8c8877bd6eb2c02fc7df215180e09fea899
thetong <ukkharawat.uim@gmail.com>
2020-03-30 15:46:49 +0700

    if user is not staff init excluding id with their id

--- tucovid/account/api_views.py
+++ tucovid/account/api_views.py
@@ -14,14 +14,17 @@ def search_relation_profile_api(request):
     profiles = search_profile(keyword)
     response = []
 
-    reporter_id = None
-    
-    if body['reporter_id']:
-        reporter_id = int(body['reporter_id'])
-        profiles = profiles.exclude(user__pk=reporter_id)
+    reporter_id = None if request.user.is_staff else user.pk
 
-    for profile in profiles:
-        if not Relationship.objects.filter(persons__pk=reporter_id).filter(persons__pk=profile.user.pk).exists():
+    if body['reporter_id'] and not reporter_id:
+        reporter_id = body['reporter_id']
+
+    for profile in profiles.exclude(user__pk=reporter_id):
+        if not Relationship.objects \
+            .filter(persons__pk=reporter_id) \
+            .filter(persons__pk=profile.user.pk) \
+            .exists():
+
             response.append(map_profile(profile))
 
     return JsonResponse(data=response, status=200, safe=False)
@@ -33,10 +36,10 @@ def search_profile_api(request):
     keyword = body['keyword']
 
     profiles = search_profile(keyword)
-    excluding_ids = []
+    excluding_ids = []  if request.user.is_staff else [ user.pk ]
 
     if body['excluding_ids']:
-        excluding_ids = [ int(id) for id in body['excluding_ids'] ]
+        excluding_ids += body['excluding_ids']
 
     response = [ map_profile(profile) for profile in profiles.exclude(user__pk__in=excluding_ids) ]
 
