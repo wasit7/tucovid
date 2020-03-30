c5f77e6eba91cbeb9237f3d28a9cc89e7b53ec7a
thetong <ukkharawat.uim@gmail.com>
2020-03-30 12:44:58 +0700

    add user_must_have_profile decor

--- tucovid/account/decorators.py
+++ tucovid/account/decorators.py
@@ -7,3 +7,13 @@ def is_anonymous(function):
             return redirect('relation:index')
 
     return wrap
+
+def user_must_have_profile(function):
+    def wrap(request, *args, **kwargs):
+        if hasattr(request.user, 'profile'):
+            return function(request, *args, **kwargs)
+
+        else:
+            return redirect('account:profile_page')
+
+    return wrap
