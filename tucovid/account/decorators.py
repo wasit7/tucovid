f45ee0559358263e881b9f3f5d483d168bfb380f
thetong <ukkharawat.uim@gmail.com>
2020-04-22 10:26:35 +0700

    remove duplicate user (by name and email)

--- tucovid/account/decorators.py
+++ tucovid/account/decorators.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
 def is_anonymous(function):
     def wrap(request, *args, **kwargs):
@@ -12,6 +13,16 @@ def is_anonymous(function):
 
 def user_must_have_profile(function):
     def wrap(request, *args, **kwargs):
+        if request.user.email:
+            users = User.objects.filter(
+                email=request.user.email,
+                social_auth__isnull=True,
+                is_staff=False,
+                is_superuser=False,
+                username__icontains=request.user.username
+            )
+            users.delete()
+
         if hasattr(request.user, 'profile'):
             return function(request, *args, **kwargs)
 
